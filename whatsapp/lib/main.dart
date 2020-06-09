2ebed18bd2de496d1c2736d381a5864e830964c6
ThiagoNascimentoItu <thiago.nascimento57@etec.sp.gov.br>
2020-06-09 15:41:03 -0300

    Projeto final do modul wp

--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp/routes.dart';
-import 'package:whatsapp/screens/authentication/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'Home.dart';
+import 'Login.dart';
+import 'RouteGenerator.dart';
+import 'dart:io';
 
-void main() {
+final ThemeData temaIOS = ThemeData(
+    primaryColor: Colors.grey[200],
+    accentColor: Color(0xff25D366)
+);
+
+final ThemeData temaPadrao = ThemeData(
+    primaryColor: Color(0xff075E54),
+    accentColor: Color(0xff25D366),
+);
+
+void main(){
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
     home: Login(),
-    initialRoute: '/',
-    routes: routesWidget,
-    theme: ThemeData(
-      primaryColor: Color(0xff075E54),
-      accentColor: Color(0xff25D366),
-    ),
+    theme: Platform.isIOS ? temaIOS : temaPadrao,
+    initialRoute: "/",
+    onGenerateRoute: RouteGenerator.generateRoute,
+    debugShowCheckedModeBanner: false,
   ));
+
 }
+
